dfcdb52d9c67ed650e7a044033ea7de211ed6bae
Finnik <73883383+golkity@users.noreply.github.com>
2025-06-16 19:32:53 +0300

    Update: in repository: add nickname; in domain: NickName -> Nickname

--- internal/domain/users.go
+++ internal/domain/users.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID           int64
 	Email        string
-	NickName     string
+	Nickname     string
 	PasswordHash []byte
 	Provider     string
 	ProviderID   string
