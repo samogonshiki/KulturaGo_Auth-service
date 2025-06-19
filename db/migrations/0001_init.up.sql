a4c0f33cd6ba86bf20b6f491a6132bc5fab341c9
Finnik <73883383+golkity@users.noreply.github.com>
2025-06-19 19:45:24 +0300

    Udpate: style field

--- db/migrations/0001_init.up.sql
+++ db/migrations/0001_init.up.sql
@@ -1,17 +1,18 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE users (
-  id            BIGSERIAL PRIMARY KEY,
-  email         TEXT UNIQUE,
-  nickname      TEXT UNIQUE,
-  password_hash BYTEA NOT NULL,
-  provider      TEXT DEFAULT 'local',
-  provider_id   TEXT,
-  created_at    TIMESTAMPTZ DEFAULT now()
+                       id            BIGSERIAL PRIMARY KEY,
+                       email         CITEXT  NOT NULL UNIQUE,
+                       nickname      TEXT    NOT NULL UNIQUE,
+                       password_hash BYTEA   NOT NULL,
+                       provider      TEXT    NOT NULL DEFAULT 'local',
+                       provider_id   TEXT,
+                       created_at    TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE refresh_tokens (
-  token       UUID PRIMARY KEY,
-  user_id     BIGINT REFERENCES users(id) ON DELETE CASCADE,
-  expires_at  TIMESTAMPTZ NOT NULL
+                                token       UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                                user_id     BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+                                expires_at  TIMESTAMPTZ NOT NULL
 );
