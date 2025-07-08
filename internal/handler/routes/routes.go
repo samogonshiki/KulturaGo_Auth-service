47fc355b9181ab6e4893281158dd7a2fc9ef2470
Finnik <73883383+golkity@users.noreply.github.com>
2025-07-08 18:53:39 +0300

    SCRUM-7 Update: add func for cheking time life for ttl tokens

--- internal/handler/routes/routes.go
+++ internal/handler/routes/routes.go
@@ -7,10 +7,13 @@ import (
 	"kulturago/auth-service/internal/middleware"
 	"kulturago/auth-service/internal/service"
 	"kulturago/auth-service/internal/tokens"
+	"time"
 )
 
 func NewRouter(svc *service.Service, mgr *tokens.Manager) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.SlidingRefresh(svc, mgr, 15*time.Minute))
 
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"},
@@ -33,7 +36,7 @@ func NewRouter(svc *service.Service, mgr *tokens.Manager) *chi.Mux {
 		r.Get("/api/v1/me", ah.Me)
 		r.Get("/api/v1/profile", ah.Profile)
 		r.Put("/api/v1/profile", ah.SaveProfile)
-		r.Get("/api/v1/avatar/presign", ah.PresignAvatar)
+		r.Get("/api/v1/avatar/presign", ah.PresignAvatar) //SCRUM-6
 	})
 
 	return r
