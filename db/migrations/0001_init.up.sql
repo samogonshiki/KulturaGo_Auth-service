c522ccfbbc4120797d394a3c5318e6e25b57a816
Finnik <73883383+golkity@users.noreply.github.com>
2025-07-12 16:02:23 +0300

    SCRUM-7 Update: add new field and add new table security_settings

--- db/migrations/0001_init.up.sql
+++ db/migrations/0001_init.up.sql
@@ -1,28 +1,39 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS citext;
 
-CREATE TABLE users (
-                       id            BIGSERIAL PRIMARY KEY,
-                       email         CITEXT  NOT NULL UNIQUE,
-                       nickname      TEXT    NOT NULL UNIQUE,
-                       password_hash BYTEA   NOT NULL,
-                       provider      TEXT    NOT NULL DEFAULT 'local',
-                       provider_id   TEXT,
-                       created_at    TIMESTAMPTZ NOT NULL DEFAULT now()
-);
+CREATE TABLE IF NOT EXISTS users (
+                                     id               BIGSERIAL PRIMARY KEY,
+                                     email            CITEXT        NOT NULL UNIQUE,
+                                     nickname         TEXT          NOT NULL UNIQUE,
+                                     password_hash    BYTEA         NOT NULL,
+                                     provider         TEXT          NOT NULL DEFAULT 'local',
+                                     provider_id      TEXT,
+                                     created_at       TIMESTAMPTZ   NOT NULL DEFAULT now(),
 
-CREATE TABLE profiles (
-                          user_id   BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
-                          full_name TEXT    DEFAULT '',
-                          about     TEXT    DEFAULT '',
-                          avatar    TEXT    DEFAULT '',
-                          city      TEXT    DEFAULT '',
-                          phone     TEXT    DEFAULT '',
-                          birthday  DATE
-);
+    two_fa_enabled    BOOLEAN NOT NULL DEFAULT false,
+    login_alerts      BOOLEAN NOT NULL DEFAULT false,
+    allow_new_devices BOOLEAN NOT NULL DEFAULT true
+    );
 
-CREATE TABLE refresh_tokens (
-                                token       UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                                user_id     BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-                                expires_at  TIMESTAMPTZ NOT NULL
-);
+CREATE TABLE IF NOT EXISTS profiles (
+                                        user_id   BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+    full_name TEXT    DEFAULT '',
+    about     TEXT    DEFAULT '',
+    avatar    TEXT    DEFAULT '',
+    city      TEXT    DEFAULT '',
+    phone     TEXT    DEFAULT '',
+    birthday  DATE
+    );
+
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+                                              token       UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id     BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    expires_at  TIMESTAMPTZ NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS security_settings (
+                                                 user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
+    setting_key TEXT,
+    enabled     BOOLEAN NOT NULL,
+    PRIMARY KEY (user_id, setting_key)
+    );
