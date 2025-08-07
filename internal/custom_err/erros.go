e4d8396536f652ac8404e48cf13edf8bfa842d4d
Finnik <73883383+golkity@users.noreply.github.com>
2025-08-07 19:28:19 +0300

    SCRUM-10 Update: add new errors: not an admin; invalid credentials; key must be 16,24 or 32 bytes

--- internal/custom_err/erros.go
+++ internal/custom_err/erros.go
@@ -3,6 +3,9 @@ package custom_err
 import "errors"
 
 var (
-	ErrExists       = errors.New("user exists")
-	ErrInvalidCreds = errors.New("invalid credentials")
+	ErrExists             = errors.New("user exists")
+	ErrInvalidCreds       = errors.New("invalid credentials")
+	ErrKeySize            = errors.New("key must be 16, 24 or 32 bytes")
+	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrNotAdmin           = errors.New("not an admin")
 )
