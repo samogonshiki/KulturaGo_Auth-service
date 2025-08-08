6c88ea0fe334c86634cc37498fc87f3ab8659884
Finnik <73883383+golkity@users.noreply.github.com>
2025-08-09 00:31:11 +0300

    SCRUM-10 feat(add new route & fix bugs): add routes for profile and admins routers, update global routes

--- internal/handler/routes/routes.go
+++ internal/handler/routes/routes.go
@@ -1,42 +1,77 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
-	"kulturago/auth-service/internal/handler/http"
+
+	httpH "kulturago/auth-service/internal/handler/http"
 	"kulturago/auth-service/internal/middleware"
 	"kulturago/auth-service/internal/service"
 	"kulturago/auth-service/internal/tokens"
-	"time"
 )
 
-func NewRouter(svc *service.Service, mgr *tokens.Manager) *chi.Mux {
+func NewRouter(userSvc *service.Service, adminSvc service.AdminService, mgr *tokens.Manager) *chi.Mux {
 	r := chi.NewRouter()
-
-	r.Use(middleware.SlidingRefresh(svc, mgr, 15*time.Minute))
 
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
 	}))
 
-	ah := http.NewAuthHandler(svc, mgr)
+	r.Use(middleware.SlidingRefresh(userSvc, mgr, 15*time.Minute))
+
+	uh := httpH.NewAuthHandler(userSvc, mgr)
+	ah := httpH.NewAdminHandler(adminSvc, mgr)
 
 	r.Route("/api/v1/auth", func(r chi.Router) {
-		r.Post("/signup", ah.SignUp)
-		r.Post("/signin", ah.SignIn)
-		r.Post("/refresh", ah.Refresh)
-		r.Post("/logout", ah.Logout)
+		r.Post("/signup", uh.SignUp)
+		r.Post("/signin", uh.SignIn)
+		r.Post("/refresh", uh.Refresh)
+		r.Post("/logout", uh.Logout)
+
+		r.Group(func(r chi.Router) {
+			r.Use(middleware.Auth(mgr))
+
+			r.Get("/security", uh.Security)
+			r.Patch("/security/{key}", uh.ToggleSecurity)
+			r.Put("/security/password", uh.ChangePassword)
+
+			r.Get("/me", uh.Me)
+			r.Get("/profile", uh.Profile)
+			r.Put("/profile", uh.SaveProfile)
+			r.Patch("/profile/avatar", uh.UpdateAvatar)
+			r.Get("/avatar/presign", uh.PresignAvatar)
+		})
 	})
 
-	r.Group(func(r chi.Router) {
-		r.Use(middleware.Auth(mgr))
-		r.Get("/api/v1/me", ah.Me)
-		r.Get("/api/v1/profile", ah.Profile)
-		r.Put("/api/v1/profile", ah.SaveProfile)
-		r.Get("/api/v1/avatar/presign", ah.PresignAvatar) //SCRUM-6
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Group(func(r chi.Router) {
+			r.Use(middleware.Auth(mgr))
+
+			r.Get("/profile", uh.Profile)
+			r.Put("/profile", uh.SaveProfile)
+			r.Patch("/profile/avatar", uh.UpdateAvatar)
+
+			r.Get("/security", uh.Security)
+			r.Patch("/security/{key}", uh.ToggleSecurity)
+			r.Put("/security/password", uh.ChangePassword)
+
+			r.Get("/me", uh.Me)
+			r.Get("/avatar/presign", uh.PresignAvatar)
+		})
+	})
+
+	r.Route("/api/v1/admin", func(r chi.Router) {
+		r.Post("/signin", ah.ASignIn)
+		r.Group(func(r chi.Router) {
+			r.Use(middleware.Auth(mgr))
+			r.Post("/logout", ah.ALogout)
+			r.Get("/access", ah.Aaccess)
+		})
 	})
 
 	return r
